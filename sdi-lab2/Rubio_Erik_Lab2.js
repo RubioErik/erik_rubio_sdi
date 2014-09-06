6abbfa5a7a90f944400df653b2d9ac9910060cdb
RubioErik <erikrubio2012@gmail.com>
2014-09-06 15:05:40 -0400

    sdi-lab2

---
+++ sdi-lab2/Rubio_Erik_Lab2.js
@@ -0,0 +1,29 @@
+alert("JavaScript works!");
+
+// Erik Rubio
+// SDI 1409
+// Day 2 Lab
+
+// My Variables
+
+var myCar = "Challenger";
+var myFavoriteShop = 'Pep Boy\'s';
+var numberOfTrips = 7;
+var open = true;
+
+// My confirms 
+
+open = confirm("Is the shop open today? \nChoose OK for Yes, or select cancel for No.");
+myCar = prompt("Enter your car model:");
+numberOfTrips = parseFloat(prompt("How many trips do you usually take per month?"));
+
+console.log(numberOfTrips);
+
+// My Outputs 
+
+console.log("I drive my " + myCar + ".");
+console.log("I shop at " + myFavoriteShop + " for new car parts.");
+console.log("I take about " + numberOfTrips + " trips per month.");
+console.log("It's " + open + " that it is open today");
+
+
