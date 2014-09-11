dd445d9ca21ab550cfcde67981bc4afb4a5bd2b2
RubioErik <erikrubio2012@gmail.com>
2014-09-11 16:54:55 -0400

    sdi-lab4-logic

---
+++ sdi-lab4-logic/Rubio_Erik_Logic4.js
@@ -0,0 +1,101 @@
+//  NAME:  Erik Rubio
+//  DATE:  9/11/14
+// Scalable Data Infrastructures
+// Day 4 Lab
+//  Assignment 1
+//  Using logical operators
+
+
+// TABLE CODE
+
+
+//var p;
+//var q;
+//var r;
+//var zero = 0;
+
+	//while(zero < 8) {
+	//p = confirm("First value:  Click OK for true or Cancel for false.");
+	//q = confirm("Second value:  Click OK for true or Cancel for false.");
+	//r = confirm("Third value:  Click OK for true or Cancel for false.");
+	
+	
+	//if (p || (q || r)){
+	    //console.log("With " + p + " and " + q + " and " + r + ", the outcome is TRUE.");
+	//} else {
+	    //console.log("With " + p + " and " + q + " and " + r + ", the outcome is FALSE.");
+	//};
+	
+	//zero++;
+//}
+
+
+
+// WORD PROBLEM 1 CODE
+
+
+//var ageOfPerson;
+//var movieTicket = 12;
+//var noDiscount = 12
+
+//console.log("Our tickets are normally 12 Dollars today.");
+//console.log("You could recieve your ticket today for 7 dollars based on your age.");
+ //console.log("Let's find out your age!");
+ 
+ //ageOfPerson = prompt("What is your age?");
+ 
+ //if (ageOfPerson >= 55 || ageOfPerson <= 10) {
+	// console.log("Yes! you receive the movie discount today!");
+	 //if (movieTicket = ageOfPerson) {
+	 //movieTicket = 7;
+	 //console.log("Awesome! you pay " + movieTicket + " dollars today!"); }
+			
+	 //} else {
+	 //console.log("You do not recieve the discount.");
+	
+	   //{ 
+		 //console.log("You must pay " + noDiscount + " dollars today, Sorry!");}
+	 //}  
+
+
+
+// WORD PROBLEM 2 CODE
+
+
+/*
+var frontLeft;
+var frontRight;
+var backLeft;
+var backRight;
+
+console.log("Let's check to see if your tires are good to go.");
+console.log("We will ask for the tire pressures in your back and front tires.");
+
+
+
+var frontLeft = prompt("What is the pressure of your front left tire?", "25  or 50 PSI");
+var frontRight = prompt("What is the pressure of your front right tire?", "25 or 50 PSI");
+var backLeft = prompt("What is the pressure of your back left tire?", " 25 or 50 PSI");
+var backRight = prompt("What is the pressure of your back right tire?", " 25 or 50 PSI");
+
+if (frontLeft != frontRight || backLeft != backRight) { 
+	
+	console.log("Either your back or front tires are not even with each other please get them checked.");
+	
+	
+} else { 
+	
+	console.log("Your 2 front and 2 back tires are even with each other and are good to go!");
+	
+	}*/
+	
+
+
+
+
+
+
+
+
+	
+
