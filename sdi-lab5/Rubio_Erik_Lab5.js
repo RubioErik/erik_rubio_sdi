5d82c66d48df49ac5f14e4e58e0aed504101328f
RubioErik <erikrubio2012@gmail.com>
2014-09-13 16:26:55 -0400

    final Rubio_Erik_Lab5.js

--- sdi-lab5/Rubio_Erik_Lab5.js
+++ sdi-lab5/Rubio_Erik_Lab5.js
@@ -4,34 +4,57 @@
 // SDI 1409
 // Lab 5
 
-// Addition Function
+// divde function
 
 var firstNumber;
 var secondNumber;
 var operation;
 var returnedNumber;
  
+var divide   = function (first, second) {
+	
+	var mySolution4;
+	var mySolution4 = (firstNumber / secondNumber);
+	if(operation == "/") {
+		console.log(firstNumber / secondNumber);
+	}
+	return mySolution4;
+	
+};
+
+// multiply function
+
 var multiply = function (first, second) {
 	
 	var mySolution3;
 	var mySolution3 = (firstNumber * secondNumber);
-	
+	if(operation == "*") {
+		console.log(firstNumber * secondNumber);
+	}
 	return mySolution3;
 	
 };
+// subtraction function
 
 var subtraction = function(first, second) {
 	
 	var mySolution2;
 	var mySolution2 = (firstNumber - secondNumber);
-	
+	if(operation == "-") {
+		console.log(firstNumber - secondNumber);
+	}
 	return mySolution2;
 };
+
+// addition function
+
 var addition = function (first, second) {
 	
 	var mySolution;
 	var mySolution = (firstNumber + secondNumber);
-	
+	if(operation == "+") {
+		console.log(firstNumber + secondNumber);
+	}
 	return mySolution;
 	
 
@@ -39,13 +62,14 @@ var addition = function (first, second) {
 returnedNumber = addition();
 returnedNumber = subtraction();
 returnedNumber = multiply();
+returnedNumber = divide();
 
 firstNumber = parseFloat(prompt("What is the first number?"));
 console.log(firstNumber);
 
 operation = prompt("Select your operation, type +,-,*, or /");
 console.log(operation);
-
+	
 secondNumber = parseFloat(prompt("What is the second number?"));
 console.log(secondNumber);
 
@@ -53,5 +77,7 @@ console.log(secondNumber);
 returnedNumber = addition(firstNumber, secondNumber);
 returnedNumber = subtraction(firstNumber, secondNumber);
 returnedNumber = multiply(firstNumber, secondNumber);
-console.log("= " + returnedNumber);
+returnedNumber = divide(firstNumber, secondNumber);
+
+
 
