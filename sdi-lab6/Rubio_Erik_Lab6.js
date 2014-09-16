9fbe84e030c0ab568c8321363ff3714836db8f67
RubioErik <erikrubio2012@gmail.com>
2014-09-16 16:09:55 -0400

    Rubio_Erik_Lab6.js

---
+++ sdi-lab6/Rubio_Erik_Lab6.js
@@ -0,0 +1,31 @@
+//alert("JavaScript works!");
+
+
+// Erik Rubio
+// SDI 1409 
+// Lab 6 
+
+
+
+var function6 = function() {
+	
+	var favSportTeams = ["Rangers", "Cowboys", "Mavericks"];	
+	var sportType = ["in Baseball are number 1", "in Football are number 7  ", "in Basketball are number 3 "];
+	
+	favSportTeams.push("Stars");
+	sportType.push("in Hockey are number 10");
+	
+	
+
+	for (var i = 0; i < favSportTeams.length; i++) {
+
+		
+		
+		console.log(favSportTeams[i] + " " + sportType[i]);
+		
+	};
+	
+};
+
+function6();
+
