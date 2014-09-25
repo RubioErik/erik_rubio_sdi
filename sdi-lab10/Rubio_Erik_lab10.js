613013f838b97b13cb4852343170fc3dad2db954
RubioErik <erikrubio2012@gmail.com>
2014-09-25 15:18:32 -0400

    first_push_lab10

---
+++ sdi-lab10/Rubio_Erik_lab10.js
@@ -0,0 +1,24 @@
+//alert("JavaScript works!");
+
+// Erik Rubio
+// SDI 1409
+// Lab 10
+
+
+// Problem 2
+
+var pBall = function() {
+	
+	var powerBall = [];
+	
+	for(i = 0; i < 4; i++){
+		
+		powerBall= Math.floor(Math.random([i]) *59);
+		console.log(powerBall);
+		
+	}
+	powerBall.push(Math.floor(Math.random([i]) *35));	
+	
+};
+
+pBall();
